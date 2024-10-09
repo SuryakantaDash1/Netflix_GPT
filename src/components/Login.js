991f59f8182716fed89680cfd2dfe89b0106c0bd
Suryakanta Dash <sdash8273@gmail.com>
2024-10-09 16:50:23 +0530

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,10 @@ const Login = () => {
   return (
     <div>
         <Header/>
-        <div>
+        <div className='absolute'>
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg" alt="Background_image" />
         </div>
-        <form>
+        <form className=' relative bg-black p-4 w-3/12'>
             <input type="text" placeholder='Email'className='p-2 m-2' />
             <input type="password" placeholder='Password' className='p-2 m-2' />
             <button className='p-4 m-4'>Sign in</button>
