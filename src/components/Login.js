c497384dc0186532f4a98578f5d13c8a307a8997
Suryakanta Dash <sdash8273@gmail.com>
2024-10-10 11:07:26 +0530

    completed login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,12 @@ const Login = () => {
         <div className='absolute'>
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg" alt="Background_image" />
         </div>
-        <form className=' relative bg-black p-4 w-3/12'>
-            <input type="text" placeholder='Email'className='p-2 m-2' />
-            <input type="password" placeholder='Password' className='p-2 m-2' />
-            <button className='p-4 m-4'>Sign in</button>
+        <form className='absolute bg-black p-4 w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+            <h1 className='text-3xl font-bold py-4 '>Sign In</h1>
+            <input className='p-2 my-4 w-full bg-gray-700' type="text" placeholder='Email' />
+            <input className='p-2 my-4 w-full bg-gray-700' type="password" placeholder='Password'  />
+            <button className='p-4 my-4 bg-red-600 w-full rounded-lg'>Sign in</button>
+            <p className='py-4'>New to Netflix? Sign Up Now</p>
         </form>
     </div>
   )
