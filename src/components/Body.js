31f9c87790ab9d7f1218ebc663731b6f892ec977
Suryakanta Dash <sdash8273@gmail.com>
2024-10-19 09:08:11 +0530

    added firebase

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,9 @@ import Login from './Login';
 import Browse from './Browse';
 import {RouterProvider} from 'react-router-dom';
 import {createBrowserRouter} from 'react-router-dom';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
 
 const Body = () => {
 
@@ -16,7 +19,19 @@ const Body = () => {
             element: <Browse/>
         }
 
-    ])
+    ]);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              const uid = user.uid;
+              
+            } else {
+              // User is signed out
+              // ...
+            }
+          });
+    }, []);
   return (
     <div>
         <RouterProvider router={appRouter}/>
