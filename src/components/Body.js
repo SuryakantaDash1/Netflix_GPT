1ae10bbef5602885f590efad87397a16344676a0
Suryakanta Dash <sdash8273@gmail.com>
2024-10-19 09:13:03 +0530

    updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,8 +6,11 @@ import {createBrowserRouter} from 'react-router-dom';
 import { useEffect } from 'react';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
+    const dispatch = useDispatch();
 
     const appRouter = createBrowserRouter([
         {
@@ -24,8 +27,8 @@ const Body = () => {
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
-              const uid = user.uid;
-              
+              const {uid, email, displayName} = user;
+              dispatch(addUser({uid: uid, email: email, displayName: displayName}));
             } else {
               // User is signed out
               // ...
